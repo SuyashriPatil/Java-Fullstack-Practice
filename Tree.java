2e406757fb98ff737d466a9c2e0174a35191a1fa
Suyashri Satappa Patil <suyashripatil@gmail.com>
2026-05-10 13:17:36 +0530

    Add files via upload

---
+++ Tree.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tree;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.filechooser.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+/**
+ *
+ * @author admin
+ */
+public class Tree {
+
+    public static void main(String[] args) {
+        
+        JFrame jf=new JFrame("Main");
+        jf.setSize(400,400);
+        jf.setVisible(true);
+        
+        Image icon=Toolkit.getDefaultToolkit().getImage("C:\\Users\\admin\\Pictures\\linkdin.png");
+        jf.setIconImage(icon);
+//        JFileChooser jf=new JFileChooser("c:");
+//        jf.showSaveDialog(null);
+//        
+
+//        DefaultMutableTreeNode d1=new DefaultMutableTreeNode("Style");
+//        DefaultMutableTreeNode d2=new DefaultMutableTreeNode("Color");
+//        DefaultMutableTreeNode d3=new DefaultMutableTreeNode("Font");
+//        d1.add(d2);
+//        d1.add(d3);
+//        DefaultMutableTreeNode d4=new DefaultMutableTreeNode("Red");
+//        DefaultMutableTreeNode d5=new DefaultMutableTreeNode("Green");
+//        DefaultMutableTreeNode d6=new DefaultMutableTreeNode("White");
+//        DefaultMutableTreeNode d7=new DefaultMutableTreeNode("Black");
+//        d2.add(d4);
+//        d2.add(d5);
+//        d2.add(d6);
+//        d2.add(d7);
+//        
+//        JTree jt=new JTree(d1);
+//        jf.add(jt);
+
+        
+        
+    }
+}
